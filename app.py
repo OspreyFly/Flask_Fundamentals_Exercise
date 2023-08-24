88d069a5f02f761014207bdda26a3eb1e8357528
Noah Goff <126897236+OspreyFly@users.noreply.github.com>
2023-08-24 15:45:16 -0500

    Add files via upload

--- app.py
+++ app.py
@@ -1,53 +1,14 @@
-# Put your app in here.
-from flask import Flask, request
+from flask import Flask
 app = Flask(__name__)
 
-@app.route('/add')
-def add():
-    a = int(request.args.get('a', 0))
-    b = int(request.args.get('b', 0))
-    result = a + b
-    return str(result)
+@app.route('/welcome')
+def say_welcome():
+    return "Welcome!"
 
-@app.route('/sub')
-def subtract():
-    a = int(request.args.get('a', 0))
-    b = int(request.args.get('b', 0))
-    result = a - b
-    return str(result)
+@app.route('/welcome/home')
+def welcome_home():
+    return "Welcome Home!"
 
-@app.route('/mult')
-def multiply():
-    a = int(request.args.get('a', 0))
-    b = int(request.args.get('b', 0))
-    result = a * b
-    return str(result)
-
-@app.route('/div')
-def divide():
-    a = int(request.args.get('a', 0))
-    b = int(request.args.get('b', 1)) 
-    result = a / b
-    return str(result)
-
-@app.route('/math')
-def doMath():
-    a = int(request.args.get('a', 0))
-    b = int(request.args.get('b', 0))
-    operation = request.args.get('op', 'add')
-
-    if operation == 'add':
-        result = a + b
-    elif operation == 'sub':
-        result = a - b
-    elif operation == 'mult':
-        result = a * b
-    elif operation == 'div':
-        result = a / b
-    else:
-        return "Invalid operation"
-
-    return str(result)
-
-if __name__ == "__main__":
-    app.run()
+@app.route('/welcome/back')
+def welcome_back():
+    return "Welcome Back!"
